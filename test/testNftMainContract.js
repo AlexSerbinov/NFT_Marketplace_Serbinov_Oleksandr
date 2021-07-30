4c2f312bdf7f3fd688c69b032a3e7102f34b9b06
serbinov.oleksandr <serbinov.oleksandr@teamharbour.com>
2021-07-30 16:56:35 +0300

    before each not working

--- test/testNftMainContract.js
+++ test/testNftMainContract.js
@@ -2,13 +2,14 @@ const NFT = artifacts.require('../contracts/NFT.sol');
 const ownerAddres = '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe';
 
 contract('NFT', async () => {
+  beforeEach(async (ctx) => {
+    const nft = await NFT.new('test NFT', 'TNFT');
+    console.log(' ------------ line 0 ------------');
+  });
+  console.log(' ------------ line 0.5 ------------');
 
   it('Should mint NFT token for given user', async () => {
-
     console.log(' ------------ line 1 ------------');
-    const nft = await NFT.new('test NFT', 'TNFT');
-    console.log(' ------------ line 2 ------------');
-    
     const mint = await nft.mintNft(ownerAddres); //.then(console.log);
     expect(mint).toBeDefined();
   });
